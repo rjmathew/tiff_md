c36b670b696b75e1201466b94b8da67da7c0797b
Rajesh Mathew <rjmathew@gmail.com>
2015-06-11 21:40:01 -0700

    First commit

---
+++ TIFF_Metadata/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TIFF_Metadata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap img;
+            img = new Bitmap(@"c:\users\rajesh\downloads\marbles.tif", true);
+            int height = img.Size.Height;
+            int width = img.Size.Width;
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Color pixelColor = img.GetPixel(50, 50);
+                    int r = pixelColor.R;
+                    int g = pixelColor.G;
+                    int b = pixelColor.B;
+                }
+                
+            }
+            Console.WriteLine("done");
+        }
+    }
+}
